9fd4eceee5df9477b8561da31ead28bb8d887c9a
Rebekah Taylor <rebekahtdesigns@gmail.com>
2022-10-25 09:47:58 -0500

    recipe form and directory

---
+++ src/components/forms/CreateRecipeForm.js
@@ -0,0 +1,234 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const CreateRecipeForm = () => {
+  const [films, setFilms] = useState([]);
+  const [recipeCategories, setRecipeCategories] = useState([]);
+  const [userChoices, setUserChoices] = useState([
+    {
+      filmId: 0,
+      name: "",
+      imageUrl: "",
+      description: "",
+      ingredients: "",
+      instructions: "",
+      recipeCategoryId: 0,
+    },
+  ]);
+  const navigate = useNavigate();
+
+  const saveNewRecipe = (event) => {
+    event.preventDefault();
+
+    if (
+      userChoices.filmId &&
+      userChoices.name &&
+      userChoices.imageUrl &&
+      userChoices.description &&
+      userChoices.ingredients &&
+      userChoices.instructions &&
+      userChoices.recipeCategoryId
+    )
+      fetch(`http://localhost:8088/recipes`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userChoices),
+      }).then(navigate("/recipes"));
+  };
+
+  useEffect(() => {
+    fetch(`http://localhost:8088/films`)
+      .then((response) => response.json())
+      .then((filmArray) => {
+        setFilms(filmArray);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:8088/recipeCategories`)
+      .then((response) => response.json())
+      .then((categoryArray) => {
+        setRecipeCategories(categoryArray);
+      });
+  }, []);
+
+  return (
+    <form className="product-form">
+      <h2 className="product-form-title">Add A New Recipe</h2>
+      <h4 className="product-form-subhead">All fields required</h4>
+
+      <fieldset>
+        <div className="form-group">
+          <select
+            className="films"
+            id="film-select"
+            onChange={(event) => {
+              const copy = { ...userChoices };
+              copy.filmId = parseInt(event.target.value);
+              setUserChoices(copy);
+            }}
+          >
+            <option key={0} value="0">
+              Select Show/Movie
+            </option>
+            {films.map((film) => {
+              return (
+                <option
+                  //? diff btwn key and value here
+                  id={film.id}
+                  key={film.id}
+                  value={film.id}
+                  className="product-form-dropdown"
+                >
+                  {film.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="recipe-name" className="recipe-form-label">
+            Recipe Name:
+          </label>
+          <input
+            className="form-recipe-name-box"
+            type="text"
+            id="recipe-name"
+            value={userChoices.description}
+            placeholder="Enter brief description"
+            required
+            onChange={(event) => {
+              const copy = { ...userChoices };
+              copy.description = event.target.value;
+              setUserChoices(copy);
+            }}
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="product-name" className="product-form-label">
+            Description:
+          </label>
+          <input
+            className="form-description-box"
+            type="text"
+            id="product-name"
+            value={userChoices.name}
+            placeholder="Please enter full recipe name"
+            required
+            onChange={(event) => {
+              const copy = { ...userChoices };
+              copy.name = event.target.value;
+              setUserChoices(copy);
+            }}
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="ingredients" className="recipe-form-label">
+            Ingredients:
+          </label>
+          <input
+            className="form-ingredients-box"
+            type="text"
+            id="ingredients"
+            value={userChoices.ingredients}
+            placeholder="Enter ingredient list"
+            required
+            onChange={(event) => {
+              const copy = { ...userChoices };
+              copy.ingredients = event.target.value;
+              setUserChoices(copy);
+            }}
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="instructions" className="recipe-form-label">
+            Instructions:
+          </label>
+          <input
+            className="form-instructions-box"
+            type="text"
+            id="instructions"
+            value={userChoices.instructions}
+            placeholder="Enter instructions"
+            required
+            onChange={(event) => {
+              const copy = { ...userChoices };
+              copy.instructions = event.target.value;
+              setUserChoices(copy);
+            }}
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="imageUrl" className="recipe-form-label">
+            Image URL:
+          </label>
+          <input
+            className="form-imageUrl-box"
+            type="text"
+            id="imageUrl"
+            value={userChoices.imageUrl}
+            placeholder="Paste image URL here"
+            required
+            onChange={(event) => {
+              const copy = { ...userChoices };
+              copy.imageUrl = event.target.value;
+              setUserChoices(copy);
+            }}
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div className="form-group">
+          <div>Recipe Category</div>
+          {recipeCategories.map((recipeCategory) => {
+            return (
+              <div key={recipeCategory.id} className="recipe-form-radio">
+                <label>
+                  <input
+                    type="radio"
+                    value={recipeCategory.id}
+                    required
+                    checked = {userChoices.recipeCategoryId === recipeCategory.id}
+                    onChange={(event) => {
+                      const copy = { ...userChoices };
+                      copy.productTypeId = parseInt(event.target.value);
+                      setUserChoices(copy);
+                    }}
+                  />
+                  {recipeCategory.name}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </fieldset>
+
+      <button
+        className="recipe-form-button"
+        onClick={(event) => {
+          saveNewRecipe(event);
+        }}
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
